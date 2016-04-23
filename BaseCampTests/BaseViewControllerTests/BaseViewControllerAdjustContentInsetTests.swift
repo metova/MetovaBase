fbf2c54cc08357bb4d48b9a4c633ccc88d2a1492
Nick Griffith <nick.griffith@metova.com>
2016-04-23 12:10:11 -0500

    adding skeletons for remaining unit tests to write removing example test place holder

---
+++ BaseCampTests/BaseViewControllerTests/BaseViewControllerAdjustContentInsetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewControllerAdjustContentInsetTests.swift
+//  BaseCamp
+//
+//  Created by Nick Griffith on 4/23/16.
+//
+//
+
+import XCTest
+
+@testable import BaseCamp
+
+class BaseViewControllerAdjustContentInsetTests: XCTestCase {
+
+// TODO: Write tests around `adjustContentInset(scrollview scrollView: UIScrollView, forKeyboardWillChangeFrameNotification notification: NSNotification)` method.
+    
+}
